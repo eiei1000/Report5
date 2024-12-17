d21e0d8ed061165866d1bc6876e830efc587eb30
Eiji Kishimoto <e245739@ie.u-ryukyu.ac.jp>
2024-12-17 23:25:02 +0900

    NullPointerException example

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // String型変数strにnullを代入
+        String str = null;
+
+        // str.length()を表示しようとする（エラーが発生）
+        System.out.println(str.length());
+    }
+}
